9def8b2020906739313624b8653cbdf2f55fedfc
Micael-p <micaelpessoa5@gmail.com>
2025-03-07 10:00:12 -0300

    C.py

---
+++ C.py
@@ -0,0 +1,10 @@
+class Carro:
+    def __init__(self, marca: str, modelo: str, ano: int):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+meu_carro = Carro("Toyota", "corolla", 2020)
+
+
+print(meu_carro.marca, meu_carro.modelo, meu_carro.ano)
