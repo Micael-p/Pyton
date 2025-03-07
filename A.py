d64cd709900d2c45202c75b6ad92ac002ded36b6
Micael-p <micaelpessoa5@gmail.com>
2025-03-07 09:56:27 -0300

    A.py

---
+++ A.py
@@ -0,0 +1,10 @@
+squares = []
+for x in range(10):
+    squares.append(x**2)
+
+print(squares)
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+quadrados = [num** 2 for num in numeros]
+
+print(quadrados)
